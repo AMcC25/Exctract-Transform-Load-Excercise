e322a2bf5804a19e54576f4c7bd9d5d8951add02
nnesbitt <43296459+nnesbitt@users.noreply.github.com>
2018-12-11 22:05:54 -0500

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,12 @@
+create database etl_project;
+use etl_project;
+create table eastside (
+	id int auto_increment primary key,
+    year int, 
+    business TEXT,
+    GRADE TEXT,
+    location TEXT
+    );
+    
+    
+    
