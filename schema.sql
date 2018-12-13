4269b37b2615dd352fb1de360b01d88fa8176845
nnesbitt <43296459+nnesbitt@users.noreply.github.com>
2018-12-13 16:01:06 -0500

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -7,6 +7,38 @@ create table eastside (
     GRADE TEXT,
     location TEXT
     );
+create table westside (
+	id int auto_increment primary key,
+    year int, 
+    business TEXT,
+    GRADE TEXT,
+    location TEXT
+    ); 
+#----Counting As for Westside    
+SELECT id, COUNT(GRADE)
+FROM westside
+WHERE GRADE = 'A';
+
+#----Counting As for Eastside
+SELECT id, COUNT(GRADE)
+FROM eastside
+WHERE GRADE = 'A';
+
+#----SELECT only As from WESTIDE
+SELECT * FROM westside
+WHERE GRADE = 'A';
+
+#----Count Grade by year
+SELECT id, year, business, GRADE, location FROM eastside
+UNION ALL
+SELECT id, year, business, GRADE, location FROM westside;
+
+#----Convert Text in order to use = sign--NOT WORKING.  I think this is why we can not get all data for inspections = A. GRADE should not be TEXT but VARCHAR
+#---SELECT * FROM westside
+ALTER TABLE westside
+MODIFY COLUMN GRADE datatype(VARCHAR);
+#---SELECT CONVERT(VARCHAR, GRADE);
+
     
     
     
